0d269f9286d9c8324aecfe58e51145ae297c8910
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2019-12-20 14:33:19 +0100

    Initial Commit

---
+++ Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by Arturo Carretero Calvo on 20/12/2019.
+//  Copyright © 2019 Arturo Carretero Calvo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
